6f4f5e98916efe7c999f194b896f1dfc6222d95f
Dailenis Gonzalez <dgonzalez@lionix.org>
2019-01-14 16:03:36 -0600

    feat: render cels

--- src/components/tablero.tsx
+++ src/components/tablero.tsx
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import Celda from "./celda";
 
 class Tablero extends Component {
+  public state = {
+    celdas: [0, 1, 2, 3, 4, 5, 6, 7, 8] //9 celdas
+  };
+
+  renderCeldas = () => {
+    const { celdas } = this.state;
+    return celdas.map((celda, i) => (
+      <div key={i}>
+        <Celda value={celda} />
+      </div>
+    ));
+  };
+
   render() {
-    return <div>Soy el componente tablero</div>;
+    return (
+      <div>
+        Soy el componente tablero
+        {this.renderCeldas()}
+      </div>
+    );
   }
 }
 
