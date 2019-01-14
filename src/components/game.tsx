7b4212511aad6414479bc4b0b5f0dac902add2c8
Dailenis Gonzalez <dgonzalez@lionix.org>
2019-01-14 17:31:26 -0600

    feat: game component

---
+++ src/components/game.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Game extends Component {
+    public state  = {
+        tablero: [0,1,2,3,4,5,6,7,8]
+    }
+  render() {
+    return <div> Game Component</div>;
+  }
+}
+
+export default Game;
